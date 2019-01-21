b25ce60d9dd42090e9d28f3795ce0153acb21781
liwei <elvmx47@aliyun.com>
2019-01-21 17:54:06 +0800

    banner 增加

--- public/js/banner.js
+++ public/js/banner.js
@@ -2,9 +2,22 @@ $(function() {
 
 
   $('#bannerAdd').click(function() {
-    alert('ajax提交');
 
-    // 手动调用关闭的方法
-    $('#myModal').modal('hide');
+    $.post('/banner/add', {
+      bannerName: $('#inputEmail3').val(),
+      bannerUrl: $('#inputPassword3').val()
+    }, function(res) {
+      console.log(res);
+      if (res.code === 0) {
+        // 成功
+      } else {
+        // PS: 很多时候，真正的错误信息不会给到用户去看，
+        console.log(res.msg);
+        alert('网络异常，请稍后重试');
+      }
+
+      // 手动调用关闭的方法
+      $('#myModal').modal('hide');
+    });
   })
 })
