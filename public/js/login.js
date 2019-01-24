db44e9fde05064b50cb93f508eb273cb12273c0d
liwei <elvmx47@aliyun.com>
2019-01-24 12:05:51 +0800

    登录注册接口

---
+++ public/js/login.js
@@ -0,0 +1,59 @@
+(function () {
+  var User = function () {
+    this.btnLock = false;   // 登录按钮的锁
+    // this.userName = '';
+    // this.password = '';
+
+    this.dom = {
+      submitBtn: $('#btn'),
+      userNameInput: $('input[type=text]'),
+      passwordInput: $('input[type=password]')
+    }
+  }
+
+  User.prototype.bindDOM = function () {
+    var that = this;
+    this.dom.submitBtn.click(function () {
+      // 发送ajax 请求之前，先判断是否有锁
+      if (!that.btnLock) {
+        // 没锁的时候，加把锁，并且发请求
+        that.btnLock = true;
+        that.handleLogin();
+      }
+    })
+  }
+
+  /**
+   * 登录方法，调取ajax
+   */
+  User.prototype.handleLogin = function () {
+    var that = this;
+    $.post('/user/login', {
+      userName: this.dom.userNameInput.val(),
+      password: this.dom.passwordInput.val()
+    }, function (res) {
+      if (res.code === 0) {
+        // 登录成功
+        layer.msg('登录成功');
+        console.log(res.data);
+        // document.cookie = 
+
+        setTimeout(function () {
+          // 跳转到首页
+          window.location.href = '/';
+        }, 1000);
+      } else {
+        // 登录失败
+        layer.msg(res.msg);
+      }
+
+      // 记得 解锁
+      that.btnLock = false;
+    })
+  }
+
+
+  // 最后最后
+  var user = new User();
+  user.bindDOM();
+})();
